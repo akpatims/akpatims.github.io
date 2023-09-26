d48053d4ea7092dd0dad0d932ee19a3c9bedadd9
akpatims <138819429+akpatims@users.noreply.github.com>
2023-09-26 14:33:55 +0100

    Add files via upload

---
+++ tims.py
@@ -0,0 +1,8 @@
+import time 
+startTime=time.time()
+print("start time: ", startTime)
+print("How long will this take?")
+endTime=time.time()
+print("End time: ", endTime)
+print("End time: ", startTime-endTime)
+print("Time Elapsed", startTime-endTime)
